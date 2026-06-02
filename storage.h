96346bce7554214963ff939f59bffc052d5e4f2e
KY-s2516 <s2516@oita.kosen-ac.jp>
2026-06-02 10:50:02 +0900

    データ管理

--- storage.h
+++ storage.h
@@ -7,6 +7,9 @@
 typedef struct {
     char word[MAX_LEN];
     char meaning[MAX_LEN];
+    int level;
+    int correct;
+    int wrong;
 } Word;
 
 extern Word words[MAX_WORDS];
@@ -14,7 +17,14 @@ extern int wordCount;
 
 void loadWords();
 void saveWords();
+
 int addWord(const char *word, const char *meaning);
+int deleteWord(const char *word);
+int editWord(const char *word);
+void searchWord(const char *word);
+void listWords();
+
 Word* getRandomWord();
+Word* getReviewWord();
 
 #endif
