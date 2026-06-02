11bf6872a1e90d3edad27e59dc37262415f1349e
KY-s2516 <s2516@oita.kosen-ac.jp>
2026-06-02 11:03:59 +0900

    データ管理

--- storage.h
+++ storage.h
@@ -4,12 +4,10 @@
 #define MAX_WORDS 100
 #define MAX_LEN 50
 
-typedef struct {
+typedef struct
+{
     char word[MAX_LEN];
     char meaning[MAX_LEN];
-    int level;
-    int correct;
-    int wrong;
 } Word;
 
 extern Word words[MAX_WORDS];
@@ -19,12 +17,11 @@ void loadWords();
 void saveWords();
 
 int addWord(const char *word, const char *meaning);
-int deleteWord(const char *word);
-int editWord(const char *word);
-void searchWord(const char *word);
-void listWords();
 
 Word* getRandomWord();
-Word* getReviewWord();
+
+void listWords();
+void searchWord(const char *word);
+int deleteWord(const char *word);
 
 #endif
